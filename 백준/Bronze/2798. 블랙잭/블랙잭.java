004b248b44ea9e8fa6caae1c2e6c6886e280f68c
SeoH <140609042+seoh77@users.noreply.github.com>
2024-03-14 08:11:33 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,40 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
-public class Main {
+ public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in) ;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		String[] cardNumString = br.readLine().split(" ");
+        int[] cardNum = Stream.of(cardNumString).mapToInt(Integer::parseInt).toArray();
 		
-		int N = sc.nextInt(); 			// 카드의 개수
-		int M = sc.nextInt();			// 3장의 카드 합이 M을 넘지 않아야 한다.
-		int[] arr = new int [N] ;		// 딜러가 공개한 카드 배열 
+		int answer = 0 ;
 		
-		// 딜러가 공개한 카드 값 배열 저장
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int max = 0 ;			// M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합
-		
-		for(int i=0; i<=arr.length-2; i++) {
-			for(int j=i+1; j<=arr.length-1; j++) {
-				for(int z=j+1; z<arr.length; z++) {
-					int sum = arr[i] + arr[j] + arr[z] ;
-					
-					if (sum > M) continue ;
-					
-					max = (sum > max) ? sum : max ;
-					
-					if (max == M) break ;
+		for (int i=0; i<cardNum.length-2; i++) {
+			for (int j=i+1; j<cardNum.length-1; j++) {
+				for (int z=j+1; z<cardNum.length; z++) {
+					int sum = cardNum[i]+cardNum[j]+cardNum[z] ;
+					if (sum>answer && sum<=M) answer = sum ;
 				}
 			}
 		}
 		
-		System.out.println(max);
-		
-		sc.close();
+		System.out.println(answer);
 		
 	}
-
 }
