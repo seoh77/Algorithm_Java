74652056dc500dfe3552c4af5d28955ad409a8a4
seoh77 <seoh7dev@gmail.com>
2024-03-16 18:03:02 +0900

    [Silver IV] Title: 문자열 집합, Time: 5244 ms, Memory: 68780 KB

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,33 +1,39 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
- public class Main {
-
-	public static void main(String[] args) throws IOException {
+public class Main {
+	
+	public static void main(String[] args) {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in) ;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int n = sc.nextInt();		// 문자열의 개수
+		int m = sc.nextInt();		// 검사하는 문자열 개수
 		
-		String[] setArr = new String [N] ;
-		for (int i=0; i<setArr.length; i++) {
-			setArr[i] = br.readLine() ;
- 		}
+		// 집합 s에 문자열 저장
+		String[] sArr = new String [n] ;
+		for(int i=0; i<n; i++) {
+			sArr[i] = sc.next();
+		}
 		
-		int count = 0 ;
-		for (int i=0; i<M; i++) {
-			String search = br.readLine() ;
+		int cnt = 0 ;		// 입력된 문자열 중 총 몇 개가 집합 S에 포함되어 있는지 체크할 변
+		
+		// 문자열 검사
+		for(int i=0; i<m; i++) {
+			String check = sc.next();		// 검사하고자 하는 문자열
 			
-			for (String set : setArr) {
-				if (set.equals(search)) count++ ;
+			for(int j=0; j<sArr.length; j++) {
+				// 집합 S의 요소들을 하나씩 탐색하며 check가 포함되어 있는지 확인하고,
+				if (sArr[j].equals(check)) {
+					cnt++ ;
+					break ;		// 포함되어 있을 경우 cnt를 증가시키고 바로 종료 (시간 단축을 위함)
+				}
 			}
 		}
 		
-		System.out.println(count);
+		System.out.println(cnt);
+		
+		sc.close();
 		
 	}
+
 }
