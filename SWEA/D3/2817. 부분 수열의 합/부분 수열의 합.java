15bd766d54502b31ed07975a75eb49b2224ca6a0
SeoH <140609042+seoh77@users.noreply.github.com>
2024-03-24 14:32:08 +0900

    [D3] Title: 부분 수열의 합, Time: 186 ms, Memory: 38,124 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -3,51 +3,51 @@ import java.util.Scanner;
 
 public class Solution {
 	
-	static int N ;			// N개의 자연수 수열이 주어짐
-	static int K ;			// 합이 K가 되는 경우의 수
-	static int[] arr ;		// N개의 자연수 수열을 담아둘 배열
-	static int num ;		// 경우의 수를 계산할 변수
+	static int[] arr ;			// 자연수 배열
+	static int K ;				// 합이 K가 되어야 한다.
+	static int answer ;			// 합이 K가 되는 경우의 수
 	
 	public static void main(String[] args) throws Exception {
 		
-		 // File file = new File("src/swea_2817_부분수열의합/input.txt") ;
-		 // Scanner sc = new Scanner(file) ;
-		
+		// File file = new File("src/swea_2817_부분수열의합/input.txt") ;
+		// Scanner sc = new Scanner(file) ;
 		Scanner sc = new Scanner(System.in) ;
 		
-		int T = sc.nextInt() ;			// 테스트 케이스의 수
+		int T = sc.nextInt();			// 테스트 케이스의 수
 		
 		for(int test_case=1; test_case<=T; test_case++) {
-			
-			N = sc.nextInt() ;
-			K = sc.nextInt() ;
+			// 값 입력받기
+			int N = sc.nextInt();		// 자연수 배열의 크기
 			arr = new int [N] ;
-			num = 0 ;			// 초기화
-			
-			// N개의 자연수 수열 값 입력
+			K = sc.nextInt() ;
 			for(int i=0; i<arr.length; i++) {
 				arr[i] = sc.nextInt();
 			}
+			answer = 0 ;
 			
-			sumArr(0, 0) ;
+			combi(0, 0) ;
 			
-			System.out.printf("#%d %d\n", test_case, num);
+			System.out.printf("#%d %d\n", test_case, answer);
 			
 		}
 		
+		sc.close() ;
+		
 	}
 
-	static void sumArr(int idx, int sum) {
+	private static void combi(int idx, int curSum) {
+		if (curSum == K) {
+			answer++ ;
+			return ;
+		}
 		
-		if (idx >= arr.length) return ;			// idx 범위가 배열 크기를 벗어나면 return
+		if(curSum > K || idx >= arr.length) return ;
 		
-		if (sum + arr[idx] == K) num++ ;		// sum + arr[idx] 값이 K와 같으면 num+1
+		// 해당 idx를 넣지 않는 경우
+		combi(idx+1, curSum) ;
 		
-		if (sum + arr[idx] < K) sumArr(idx+1, sum+arr[idx]) ;	  // sum + arr[idx] < K 경우에만 sum+arr[idx] 케이스를 고려
-		
-		sumArr(idx+1, sum) ;		// 해당 idx가 포함되지 않는 경우
-		
+		// 해당 idx를 넣는 경우
+		combi(idx+1, curSum+arr[idx]) ;
 	}
-	
 
 }
