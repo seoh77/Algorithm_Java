70a18847e560eb20056063d116b3760d8900d9f3
SeoH <140609042+seoh77@users.noreply.github.com>
2024-04-14 14:23:36 +0900

    [Silver I] Title: 미로 탐색, Time: 288 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,65 +4,75 @@ import java.util.Scanner;
 
 public class Main {
 	
-	// x, y 좌표를 함께 기록하기 위해 Pair 클래스 생성
-	static class Pair {
-		int x, y ;
+	static class Path {
+		int r, c ;
 		
-		public Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
+		Path(int r, int c) {
+			this.r = r ;
+			this.c = c ;
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in) ;
 		
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		int[][] arr = new int [N+1][M+1];					// N * M 크기의 미로 -> 시작을 (1,1)로 맞추기 위해 [N+1][M+1]로 배열 생성
-		boolean[][] visited = new boolean [N+1][M+1] ;		// 방문했는지 여부를 체크할 배열
+		int[][] arr = new int [N][M] ;					// 미로 배열
+		boolean[][] visited = new boolean [N][M] ;		// 방문 체크 배열
 		
-		// 미로 정보 입력 (1 : 이동할 수 있는 칸 , 0 : 이동할 수 없는 칸)
-		for(int i=1; i<=N; i++) {
+		// 미로 정보 입력
+		for(int r=0; r<N; r++) {
 			String[] str = sc.next().split("") ;
-			for(int j=1; j<=M; j++) {
-				arr[i][j] = Integer.parseInt(str[j-1]) ;
+			for(int c=0; c<M; c++) {
+				arr[r][c] = Integer.parseInt(str[c]) ;
 			}
 		}
 		
-		Queue<Pair> queue = new LinkedList<Pair>() ;
-		
-		queue.add(new Pair(1,1)) ;			// 시작 좌표(1,1) queue에 넣기
-		visited[1][1] = true ;				// 시작 좌표(1,1) 방문했다고 표시
-		
-		// 델타배열 설정
+		// 델타배열
 		int[] dr = {-1, 1, 0, 0} ;
 		int[] dc = {0, 0, -1, 1} ;
 		
+		// 큐 생성
+		Queue<Path> queue = new LinkedList<Path>() ;
+		
+		queue.add(new Path(0, 0)) ;			// 큐에 시작위치 넣기
+		visited[0][0] = true ;				// 방문처리
+		
+		int answer = 0 ;
+		
 		while(!queue.isEmpty()) {
-			// queue에서 현재 좌표 꺼내기
-			Pair p = queue.poll() ;
+			// 큐에서 현재 위치 꺼내기
+			Path now = queue.poll() ;
+			int r = now.r ;
+			int c = now.c;
 			
-			// 4방 탐색
+			// 도착지점에 도착하면 반복종료
+			if(r == (N-1) && c == (M-1)) {
+				answer = arr[r][c] ;
+				break ;
+			}
+			
+			// 도착지점에 도착하지 않으면 델타탐색하며 최소 칸수 계산
 			for(int d=0; d<4; d++) {
-				int nx = p.x + dr[d] ;
-				int ny = p.y + dc[d] ;
+				int nr = r + dr[d] ;
+				int nc = c + dc[d] ;
 				
-				// 미로의 크기를 벗어나면 pass 
-				if (nx <= 0 || nx > N || ny <= 0 || ny > M) continue ;
+				// 배열 범위 벗어나면 pass
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue ;
 				
-				// 이미 방문했거나 이동할 수 없는 칸이라면 pass 
-				if (visited[nx][ny] || arr[nx][ny] == 0) continue ;
+				// 이미 방문한 위치거나 이동할 수 없는 곳이라면 pass
+				if (visited[nr][nc] || arr[nr][nc] == 0) continue ;
 				
-				queue.add(new Pair(nx, ny)) ;		// 큐에 이동한 좌표 넣어주기
-				visited[nx][ny] = true ;			// 이동한 좌표 방문했다고 표시 
-				arr[nx][ny] = arr[p.x][p.y] + 1;	// 한 칸 이동하였기 때문에, 이전 출발지점의 거리 + 1
+				queue.add(new Path(nr, nc)) ;		// 큐에 해당 위치 넣기 (계속 탐색해야 하므로)
+				visited[nr][nc] = true ;			// 방문처리
+				arr[nr][nc] = arr[r][c] + 1 ;		// 몇 번 이동해야 arr[nr][nc]에 도착할 수 있는지 기록
 			}
 		}
 		
-		// 도착지점(N, M)의 값 출력
-		System.out.println(arr[N][M]);
+		// 출력
+		System.out.println(answer);
 		
 		sc.close();
 		
